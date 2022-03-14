4681469f84f7afa2d40d1528e5b77319f12f65d0
Alex Constantinou <a_constantinou@hotmail.co.uk>
2022-03-14 15:43:37 +0000

    photo and description added

--- src/components/BackgroundImage/BackgroundImage.js
+++ src/components/BackgroundImage/BackgroundImage.js
@@ -1,22 +1,22 @@
-import "./BackgroundImage.css"
+import './BackgroundImage.css'
 
 const BackgroundImage = () => (
   <div
-    className="bg-image"
+    className='bg-image'
     style={{
-      backgroundImage: "url('mountains.jpg')",
-      height: "100vh",
+      backgroundImage: 'url("assets/living_room_4.jpg")',
+      height: '100vh',
     }}
   >
-    <div className="mask" style={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}>
-      <div className="header-text d-flex justify-content-center text-center">
-        <h1 className="text-white mb-3">Lynn Cottage</h1>
-        <h4 className="text-white mb-3">
+    <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
+      <div className='header-text d-flex justify-content-center text-center'>
+        <h1 className='text-white mb-3'>Lynn Cottage</h1>
+        <h4 className='text-white mb-3'>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti
           consequuntur
         </h4>
-        <a className="btn btn-outline-light btn-lg" href="#!" role="button">
-          Call to action
+        <a href='#description' className='btn btn-outline-light btn-lg' role='button'>
+          Learn More
         </a>
       </div>
     </div>
--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -1,16 +1,19 @@
 const Description = () => (
-  <main className="mt-5">
+  <main id="description" className="mt-5">
     <div className="container">
       <section id="best-features" className="text-center">
-        <h2 className="mb-5 font-weight-bold">Best Features</h2>
-
+        <h2 className="mb-3 font-weight-bold">Our Home</h2>
+        <i className="fa-solid fa-house fa-2xl pb-4"></i>
         <div className="row d-flex justify-content-center mb-4">
           <div className="col-md-8">
             <p className="grey-text">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi
-              voluptate hic provident nulla repellat facere esse molestiae ipsa
-              labore porro minima quam quaerat rem, natus repudiandae debitis
-              est sit pariatur.
+            Situated in the beautiful village of East Linton, Lynn Cottage is perfectly placed for exploring this fabulous area of coastal Scotland. The 2-bed cottage is 10 minutes’ walk to the village centre and has stunning views from the back garden over Drylaw Hill.
+            </p>
+            <p className="grey-text">
+            Whether you are golfers, walkers, cyclists or looking for family fun you will find many attractions to keep you busy. Officially the sunniest area of Scotland, East Lothian has a lot to offer, from its spectacular coastline with fantastic beaches to historic castles, world-class golf courses and hill country. All within easy reach of Edinburgh with its numerous attractions.
+            </p>
+            <p className="grey-text">
+            Alternatively, if you are looking for total relaxation and rest, this is the perfect place to be, a cosy warm cottage which has 2 great pubs (with good food) within easy walking distance.
             </p>
           </div>
         </div>
