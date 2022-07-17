3012b90e572ca44f65c74b56b60f5376ed754f02
Alex Constantinou <a_constantinou@hotmail.co.uk>
2022-07-17 19:02:46 +0100

    accordion update

---
+++ src/components/FeaturesAccordion/AccordionContent.js
@@ -0,0 +1,303 @@
+import { Accordion } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+export const accommodation = <>
+  <Accordion.Body>
+    The cottage has been refurbished in keeping with its quaint nature and has a fully equipped kitchen and bathroom. There is a private garden to the back and front of the property; patio furniture and a small BBQ allow you to dine outside during the warmer weather.
+  </Accordion.Body>
+  <Accordion.Body>
+    The living room has plenty of seating with one 3-seater sofa, one large armchair, one smaller chair and a large pouffe for sitting or putting your feet up. The ingenious dining table unfolds to seat up to 6 people comfortably. There's a wood burning stove to keep you cosy on cooler evenings and efficient gas central heating.
+  </Accordion.Body>
+  <Accordion.Body>
+    Entertainment wise, there's a flat screen TV with a BT box for you to watch mainstream channels, BBC iPlayer and other apps plus Netflix. Free high-speed Wi-Fi is also provided.
+  </Accordion.Body>
+  <Accordion.Body>
+    Overlooking the garden is the fully equipped kitchen which has a full-size electric fan oven, gas hob, microwave, Nespresso Coffee machine, full size fridge freezer and washer dryer. A wide range of utensils, crockery and glasses are provided.
+  </Accordion.Body>
+  <Accordion.Body>
+    The light, well designed bathroom has a full-size bath and electric shower with a heated towel rail and plenty shelving for toiletries.
+  </Accordion.Body>
+  <Accordion.Body>
+    The master bedroom has a comfortable King size bed with a memory foam mattress, a small sofa, built in wardrobe, chest of drawers and 2 bedside tables. The room is beautifully decorated and has a calming feel to it.
+  </Accordion.Body>
+  <Accordion.Body>
+    The second bedroom is furnished with 2 comfortable single beds, a chest of drawers, built in wardrobe and bedside table. The room has been thoughtfully decorated and looks out onto the back garden.
+  </Accordion.Body>
+  <Accordion.Body>
+    A travel cot is available for babies.
+  </Accordion.Body>
+  <Accordion.Body>
+    Outside the cottage, to the front there is a pretty garden and a driveway. Parking is also available on the road outside (no permit needed). To the back there is a secure garden with a back gate overlooking the fields and Drylaw Hill, perfect for children and dogs.
+  </Accordion.Body>
+  <Accordion.Body>
+    There is a lovely garden room with a comfortable sofa/daybed, drop leaf table and electric heater, the perfect place for your evening whisky or G & T. Garden chairs, a portable BBQ, a table and 4 chairs are also provided for you to use.
+  </Accordion.Body>
+  <Accordion.Body>
+    The cottage is on one floor; however, it isn't suitable for wheelchair access.
+  </Accordion.Body>
+  <Accordion.Body>
+    Dogs are welcome as long as they are well behaved.
+  </Accordion.Body>
+</>
+
+export const travel = <>
+  <Accordion.Body>
+    <h3>Buses</h3>
+    There are regular, direct bus services from the village to the centre of Edinburgh (every 30 minutes during peak times). These are run by Lothian Buses (Dunbar to Edinburgh) and Border Buses (Berwick upon Tweed to Edinburgh). Please check the links for latest timings:
+  </Accordion.Body>
+  <Accordion.Body>
+    <a href='https://www.lothianbuses.com/wp-content/uploads/2021/08/X7_210912.pdf' target='_blank' rel='noopener noreferrer'>
+      Lothian Buses
+    </a>
+    <br/>
+    <a href='https://www.bordersbuses.co.uk/services/BORD/253?date=2022-02-02&direction=outbound' target='_blank' rel='noopener noreferrer'>
+      Border Buses
+    </a>
+  </Accordion.Body>
+  <Accordion.Body>
+    There is also a service from the village to North Berwick (every 2 hours at present. It's worth taking this bus on a clear day for the fabulous views of the countryside and coast:
+  </Accordion.Body>
+  <Accordion.Body>
+    <h3>Rail</h3>
+    Plans for the railway station in East Linton to reopen are in progress and scheduled for some time in 2023. This will provide long awaited direct services to Edinburgh and Newcastle.
+  </Accordion.Body>
+  <Accordion.Body>
+    The nearest railway station at present is Drem (a 10-minute drive) where there is a regular service to Edinburgh. For mainline services, Dunbar and Edinburgh serve the LNER service from London Kings Cross and the Cross Country train services to Plymouth.
+  </Accordion.Body>
+  <Accordion.Body>
+    <h3>Air</h3>
+    The nearest airport is Edinburgh, you can get to the cottage by taking the tram into Princes Street in Edinburgh, then taking the X7 bus to East Linton.
+  </Accordion.Body>
+</>
+
+export const town = <>
+  <Accordion.Body>
+    East Linton is a picturesque country village located between Haddington and Dunbar. The village is situated on the River Tyne, 22 miles east of Edinburgh. It's one of the prettiest villages in East Lothian, steeped in history and surrounded by some of the oldest buildings in Scotland. It remains a vibrant community with great shops, a couple of good pubs, beautiful countryside, and good transport links to surrounding towns and Edinburgh.
+  </Accordion.Body>
+  <Accordion.Body>
+    A bridge was built over the River Tyne during the 16th Century, making East Linton a staging post on the Great Northern Road between Edinburgh and London. The village developed over the years due to its location and the name Linton is most probably derived from the village waterfall, the Linn (or Lynn).
+  </Accordion.Body>
+  <Accordion.Body>
+    The centre point of the village is the distinctive fountain with its four cherubs, built in 1882 and recently refurbished. Behind the fountain is St Andrew's Church, now a place of residence. The large clock in the tower known locally as <b>Auld Jessie</b> was installed to commemorate Queen Victoria's Jubilee and is kept in fine working order.
+  </Accordion.Body>
+  <Accordion.Body>
+    At one time the village was full of working mills which were powered by the River Tyne, these included linen and flour mills, plus a distillery. <b>Preston Mill</b> which was one of these mills was built around 1599 and is one of the main historical attractions, featuring in recent times in the American hit TV series <b>Outlander</b> (visitors can still see working machinery on a tour during the summer months). <b>Phantassie Doo'cot</b>, nearby is a beehive shaped dovecot from the 16th century.
+  </Accordion.Body>
+  <Accordion.Body>
+    East Linton also had its own <b>livestock market</b>, a distinctive octagonal building which can still be seen today if you visit The Mart to buy your groceries or attend the regular weekend Craft Market.
+  </Accordion.Body>
+  <Accordion.Body>
+    The remaining working parish church, <b>Prestonkirk</b> was built in the 18th Century but retains its original chancery dating from the 13th century. One of the graves in the church yard belongs to Andrew Meikle who was the inventor of the threshing machine.
+  </Accordion.Body>
+  <Accordion.Body>
+    Another famous Lintonian was John Rennie, the civil engineer who was born at Phantassie in 1761. John started working as a millwright with Andrew Meikle then studied at Edinburgh University. In 1791 he moved to London and is most famous for building Waterloo Bridge and London Bridge.
+  </Accordion.Body>
+  <Accordion.Body>
+    East Linton has a long tradition of landscape painting. In the later 19th century East Linton produced several artists of renown known as the <b>East Linton School</b>.
+  </Accordion.Body>
+</>
+
+export const golf = <>
+  <Accordion.Body>
+    Known as Scotland's Golf Coast the area has the greatest concentration of Championship links golf courses in the world and is rated as being the best in Scotland. Numerous famous courses are on the doorstep, Muirfield, Gullane and North Berwick to name a few. With twenty-one links and parkland courses, East Lothian is a golfer's paradise. Descriptions below are in order of proximity to Lynn Cottage:
+  </Accordion.Body>
+  <Accordion.Body>
+    <h3>Winterfield (Dunbar) - 5.8 miles</h3>
+    <a href='https://winterfieldgc.com' target='_blank' rel='noopener noreferrer'>
+      winterfieldgc.com
+    </a>
+    <br/>
+    01368 863562
+  </Accordion.Body>
+  <Accordion.Body>
+    <h3>Glen (North Berwick East Links) - 6.6 miles</h3>
+    <a href='https://glengolfclub.co.uk' target='_blank' rel='noopener noreferrer'>
+      glengolfclub.co.uk
+    </a>
+    <br/>
+    01620 892726
+  </Accordion.Body>
+  <Accordion.Body>
+    <h3>North Berwick (West Links) - 6.9 miles</h3>
+    <a href='https://www.northberwickgolfclub.com' target='_blank' rel='noopener noreferrer'>
+      northberwickgolfclub.com
+    </a>
+    <br/>
+    01620 892135
+  </Accordion.Body>
+  <Accordion.Body>
+    <h3>Dunbar - 7.4 miles</h3>
+    <a href='https://dunbargolfclub.com' target='_blank' rel='noopener noreferrer'>
+      dunbargolfclub.com
+    </a>
+    <br/>
+    01620 892135
+  </Accordion.Body>
+  <Accordion.Body>
+    <h3>Luffness - 9 miles</h3>
+    <a href='https://luffnessnew.com' target='_blank' rel='noopener noreferrer'>
+      luffnessnew.com
+    </a>
+    <br/>
+    01620 843336
+  </Accordion.Body>
+  <Accordion.Body>
+    <h3>Muirfield - 9.3 miles</h3>
+    <a href='https://muirfield.org.uk' target='_blank' rel='noopener noreferrer'>
+      muirfield.org.uk
+    </a>
+    <br/>
+    01620 842123
+  </Accordion.Body>
+  <Accordion.Body>
+    <h3>Archerfield - 9.4 miles</h3>
+    <a href='https://archerfieldgolfclub.com' target='_blank' rel='noopener noreferrer'>
+      archerfieldgolfclub.com
+    </a>
+    <br/>
+    01620 897050
+  </Accordion.Body>
+  <Accordion.Body>
+    <h3>Gullane No.2 - 9.9 miles</h3>
+    <a href='https://gullanegolfclub.co.uk' target='_blank' rel='noopener noreferrer'>
+      gullanegolfclub.co.uk
+    </a>
+    <br/>
+    01620 842255
+  </Accordion.Body>
+  <Accordion.Body>
+    <h3>Kilspindie (Aberlady) - 10 miles</h3>
+    <a href='https://kilspindiegolfclub.co.uk' target='_blank' rel='noopener noreferrer'>
+      kilspindiegolfclub.co.uk
+    </a>
+    <br/>
+    01875 870358
+  </Accordion.Body>
+  <Accordion.Body>
+    <h3>Gifford - 10 mile</h3>
+    <a href='https://giffordgolfclub.com' target='_blank' rel='noopener noreferrer'>
+      giffordgolfclub.com
+    </a>
+    <br/>
+    01620 810591
+  </Accordion.Body>
+  <Accordion.Body>
+    <h3>Longniddry - 11 mile</h3>
+    <a href='https://longniddrygolfclub.co.uk' target='_blank' rel='noopener noreferrer'>
+      longniddrygolfclub.co.uk
+    </a>
+    <br/>
+    01875 852141
+  </Accordion.Body>
+  <Accordion.Body>
+    <h3>Craigielaw (Longniddry) - 11 miles</h3>
+    <a href='https://www.craigielawgolfclub.com' target='_blank' rel='noopener noreferrer'>
+      craigielawgolfclub.com
+    </a>
+    <br/>
+    01875 870800
+  </Accordion.Body>
+  <Accordion.Body>
+    <h3>Castle Park (Gifford) - 12 miles</h3>
+    <a href='https://castleparkgolfclub.co.uk' target='_blank' rel='noopener noreferrer'>
+      castleparkgolfclub.co.uk
+    </a>
+    <br/>
+    01620 810733
+  </Accordion.Body>
+  <Accordion.Body>
+    <h3>Royal Musselburgh (Prestonpans) - 16 miles</h3>
+    <a href='https://www.royalmusselburgh.co.uk' target='_blank' rel='noopener noreferrer'>
+      royalmusselburgh.co.uk
+    </a>
+    <br/>
+    01875 819000
+  </Accordion.Body>
+  <Accordion.Body>
+    <h3>Musselburgh Links - 16 miles</h3>
+    <a href='https://musselburgholdlinks.co.uk' target='_blank' rel='noopener noreferrer'>
+      musselburgholdlinks.co.uk
+    </a>
+    <br/>
+    0131 653 5122
+  </Accordion.Body>
+  <Accordion.Body>
+    <h3>Musselburgh - 18 miles</h3>
+    <a href='https://www.themusselburghgolfclub.com' target='_blank' rel='noopener noreferrer'>
+      themusselburghgolfclub.com
+    </a>
+    <br/>
+    0131 665 2005
+  </Accordion.Body>
+</>
+
+export const beaches = <>
+  <Accordion.Body>
+    <h3>Ravensheugh Sands - 4 miles</h3>
+    Part of the John Muir Country Park, Tyninghame Links, otherwise known as Ravensheugh is one of the finest beaches in the south of Scotland. Walking from the car park via one of the many paths which wind their way through the forest, you’ll come out onto the huge sandy unspoilt bay with views of the Bass Rock, the Isle of May and Fife. Great for walking at all times of year and for a picnic in the summer. Pop into Tyninghame Smithy on the way back for a light lunch or great cakes.
+  </Accordion.Body>
+  <Accordion.Body>
+    <h3>Bellhaven Bay - 5.4 miles</h3>
+    Part of the John Muir Country Park, this is a spectacular beach with low sheltering sand dunes, salt marsh and grasslands. Great for walking and popular with surfers (you can book lessons at Belhaven Surf School - <a href='https://c2csurfschool.com/' target='_blank' rel='noopener noreferrer'>
+    c2csurfschool.com
+    </a> ), this is one of the most photographed beaches in the area with its famous 'Bridge to Nowhere'.
+  </Accordion.Body>
+  <Accordion.Body>
+    <h3>Milsey or North Berwick East Beach - 6.4 miles</h3>
+    Great family beach near the town centre and the Scottish Seabird Centre, overlooking The Bass Rock and Fife. There is a paddling pool built into the rocks providing a safe area for children to splash around.
+  </Accordion.Body>
+  <Accordion.Body>
+    <h3>North Berwick West Beach - 6.4 miles</h3>
+    This long stretch of sand overlooks the island of Craigleith. The beach is parallel to the main street and is just in front of a putting green that is open during the summer.
+  </Accordion.Body>
+  <Accordion.Body>
+    <h3>Yellowcraig - 8.3 miles</h3>
+    With spectacular views over to the 1885 lighthouse on Fidra Island which is said to be the inspiration for Robert Louis Stevenson's book Treasure Island, the beach is very popular with families. There is an adventure playpark, a great network of nature trails and a BBQ area.
+  </Accordion.Body>
+  <Accordion.Body>
+    Information about the above beaches and others further afield can be found on the link below:
+    <br/>
+    <a href='https://visiteastlothian.org/explore/coast/coastal-car-parks' target='_blank' rel='noopener noreferrer'>
+      visiteastlothian.org/explore/coast/coastal-car-parks
+    </a>
+  </Accordion.Body>
+</>
+
+export const walking = <>
+  <Accordion.Body>
+    Walking trails crisscross the area, the John Muir Way and Saltire Trail going right through the village. There are also many fascinating walks near and around East Linton itself, including routes to Pencraig Brae, Smeaton Lake, Hailes Castle and Tyninghame. For more information on these and others and to download a route map, please follow the link below:
+  </Accordion.Body>
+  <Accordion.Body>
+    <a href='https://elcv.org.uk/wp-content/uploads/2020/08/Walks-Around-East-Linton-v2.pdf' target='_blank' rel='noopener noreferrer'>
+      www.elcv.org.uk/wp-content/uploads/2020/08/Walks-Around-East-Linton-v2.pdf
+    </a>
+  </Accordion.Body>
+  <Accordion.Body>
+    The walk from the village to <b>Traprain Law</b> will take you along the banks of the River Tyne, past Hailes Castle then over farmland to one of the most distinctive landmarks in the county. Once home to the Iron Age Votodini tribe and occupied up to the Roman withdrawal, you can still see remains of dwellings at the top. The largest hoard of Roman silver outside the Roman Empire was discovered here in 1919. If you don’t want to walk all the way from the village there is a car park at the foot of the law.
+  </Accordion.Body>
+  <Accordion.Body>
+    <a href='https://elcv.org.uk/wp-content/uploads/2018/02/1P-East-Linton-to-Traprain-Law.pdf' target='_blank' rel='noopener noreferrer'>
+      www.elcv.org.uk/wp-content/uploads/2018/02/1P-East-Linton-to-Traprain-Law.pdf
+    </a>
+  </Accordion.Body>
+  <Accordion.Body>
+    One of Scotland's Great Trails, the <b>John Muir Way</b> has routes in East Lothian suitable for both walkers and cyclists offering a fantastic sample of its landscapes, history, and heritage. The North Berwick to Dunbar section takes you through some of the best views the county has to offer. The middle of this section goes right past the cottage, and you can walk either the <b>East Linton to North Berwick</b> (climbing <b>North Berwick Law</b> on the way if you wish) or the <b>East Linton to Dunbar</b> section and get the bus back to the village. Check out the route on the link below:
+  </Accordion.Body>
+  <Accordion.Body>
+    <a href='https://johnmuirway.org/route/north-berwick-dunbar' target='_blank' rel='noopener noreferrer'>
+      johnmuirway.org/route/north-berwick-dunbar/
+    </a>
+  </Accordion.Body>
+  <Accordion.Body>
+    The nearby <b>Lammermuir hills</b> are a must for walkers and offer a strikingly different landscape to the coast. <b>The Herring Road</b> is the most famous old route across the Lammermuirs. When the herring industry was at its peak in the 18th and 19th century, the route was used by fishwives carrying huge creels of herring from Dunbar to the markets in Lauder, and by those travelling to Dunbar to get a stock of salted fish for the winter. <b>The Southern Upland Way</b> follows the Herring Road between Lauder and the Watch Water Reservoir. Check out the links below for these and other trails in the Lammermuirs:
+  </Accordion.Body>
+  <Accordion.Body>
+    <a href='https://www.lammermuirlife.co.uk/visiting/what-to-do/walking-routes' target='_blank' rel='noopener noreferrer'>
+      lammermuirlife.co.uk/visiting/what-to-do/walking-routes
+    </a>
+    <br/>
+    <a href='http://www.elcv.org.uk/wp-content/uploads/2018/02/Paths-in-and-Around-the-Lammermuirs.pdf' target='_blank' rel='noopener noreferrer'>
+      elcv.org.uk/wp-content/uploads/2018/02/Paths-in-and-Around-the-Lammermuirs.pdf
+    </a>
+  </Accordion.Body>
+</>
