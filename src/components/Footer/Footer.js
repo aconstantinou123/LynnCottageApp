531b4a0b3285cd3a81af092158ba66c526c12c68
Alex Constantinou <a_constantinou@hotmail.co.uk>
2023-09-10 14:48:23 +0100

    short term license info added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <footer>
       <div className='footer-copyright text-center py-3 mt-5' style={{ color: '4f4f4f', backgroundColor: 'rgba(38, 38, 38)' }}>
-        &copy; {year};
+        Short Term Let License Number: EL00085F - EPC Rating D &copy; {year};
         <a href='https://lynncottage.co.uk' style={{ color: 'white' }}>
           {' '}
           lynn-cottage.co.uk
